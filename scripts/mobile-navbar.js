d6249a64222dce996c7525fe422399c3c43323a4
AndersonSantosDias <andersonds.dev.com>
2025-10-18 12:11:50 -0300

    fix: adição de skill back-end

--- scripts/mobile-navbar.js
+++ scripts/mobile-navbar.js
@@ -10,11 +10,8 @@ function toggleMenu() {
     btnMenu.setAttribute('aria-expanded', isExpanded);
 }
 
-// Adiciona o evento de clique ao botão do menu
 btnMenu.addEventListener('click', toggleMenu);
 
-// Adiciona um evento de clique a cada link do menu mobile
-// para fechar o menu ao navegar para uma seção
 navLinksMobile.forEach(link => {
     link.addEventListener('click', () => {
         if (menuDiv.classList.contains('abrir')) {
